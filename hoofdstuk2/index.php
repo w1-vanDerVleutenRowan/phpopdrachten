6e44b8bd7dcc32f5bca8796fec80fc4c991617fd
Rowan van der Vleuten <rowan.vandervleuten@edu-kw1c.nl>
2020-02-14 11:56:52 +0100

    Opdracht 2.1 PHP periode 3

---
+++ hoofdstuk2/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * User: Rowan van der Vleuten
+ * Date: 7-2-2020
+ * Time: 14:04
+ * File: index.php
+ */
+?>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    <link href="hoofdstuk2/style.css" rel="stylesheet" type="text/css">
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
---
+++ hoofdstuk2/opdracht2.1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: Rowan van der Vleuten
+ * Date: 7-2-2020
+ * Time: 14:04
+ * File: opdracht2.1.php
+ */
+?>
+<header>
+    <h1><?php echo "IO1D4"; ?></h1>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</header>
+<aside>
+    <p><?php echo "Hello World!"; ?></p>
+    <a href=index.php>Terug</a>
+</aside>
+
+</main>
