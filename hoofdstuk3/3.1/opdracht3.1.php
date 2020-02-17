059d7c27ae973a57e53697e0beb6fa3d38077627
Rowan van der Vleuten <rowan.vandervleuten@edu-kw1c.nl>
2020-02-17 12:15:17 +0100

    Opdracht 3.1 PHP periode 3

---
+++ hoofdstuk3/3.1/opdracht3.1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: Rowan van der Vleuten
+ * Date: 17-2-2020
+ * Time: 11:28
+ * File: opdracht3.1.php
+ */
+?>
+<?php
+$evenement = "Elfstedentocht";
+$fries = "Alvestedêtocht";
+$kilometer = "200";
+$lang = "schaatstocht";
+$ijs = "natuurijs";
+$vereniging = "Koninklijke Vereniging De Friesche Elf Steden";
+$stad = "Leeuwarden";
+$provincie = "Friesland";
+$tocht = "Elfstedentocht";
+$hoevaak = "15";
+$jaar = "1909";
+$keer = "1";
+$verhaal = "De " . $tocht . " (Fries: " . $fries . " is een " . $kilometer .
+    " lange " . $lang . " over " . $ijs . " die wordt georganiseerd door de "
+. $vereniging . ". " . $stad . ", de hoofdstad van " . $provincie . ", is 
+start en aankomstplaats. De " . $tocht . " is inmiddels " . $hoevaak . " 
+verreden en werd voor het eerst in " . $jaar . " gereden en wordt maximaal "
+ . $keer . " per winter gehouden"
+?>
+<header>
+
+</header>
+<aside>
+    <p>
+        <?php echo $verhaal; ?>
+    </p>
+</aside>
+<main>
+
+</main>
